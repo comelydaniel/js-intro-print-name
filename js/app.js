e0c497d154d9cbef156a7285235e2af980c9b7e4
comelydaniel <comelydaniel2002@gmail.com>
2022-06-25 14:58:44 +0100

    ft:page with printed name

---
+++ js/app.js
@@ -0,0 +1,8 @@
+const fullName = document.getElementById('name');
+const height = document.getElementById('height');
+const country = document.getElementById('country');
+
+fullName.innerHTML = `Name: Comely Aniedi Daniel`;;
+height.innerHTML = `Height: 153cm`;
+country.innerHTML = `Country: Nigeria`
+
